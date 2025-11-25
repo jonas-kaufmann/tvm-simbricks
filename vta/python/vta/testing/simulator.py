e263b57fd82dca702261d7dc24b26e56be80d64f
Jonas Kaufmann <jkaufman@mpi-sws.org>
2025-11-25 15:59:05 +0000

    vta/.../rpc_server.py: support simulators

--- vta/python/vta/testing/simulator.py
+++ vta/python/vta/testing/simulator.py
@@ -58,7 +58,6 @@ def _load_sw():
         f = tvm.get_global_func("vta.tsim.init")
         m = tvm.runtime.load_module(lib_hw[0], "vta-tsim")
         f(m)
-        return lib_hw
 
     return libs
 
