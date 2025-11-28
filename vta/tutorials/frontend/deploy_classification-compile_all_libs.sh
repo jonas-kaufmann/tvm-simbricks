5f868216f38eb196442c508fd275625bbfacb271
Jonas Kaufmann <jkaufman@mpi-sws.org>
2025-11-28 10:21:02 +0000

    classification-compile: compile for multiple VTA block sizes

--- vta/tutorials/frontend/deploy_classification-compile_all_libs.sh
+++ vta/tutorials/frontend/deploy_classification-compile_all_libs.sh
@@ -2,11 +2,38 @@
 set -eux
 
 WORKLOAD_OPTS=(resnet18_v1 resnet34_v1 resnet50_v1)
-INFERENCE_DEVICE_OPTS=(cpu_arm64 vta)
+INFERENCE_DEVICE_CPU_OPTS=(cpu_arm64)
+INFERENCE_DEVICE_VTA_OPTS=(vta)
+BLOCK_OPTS=(16 32)
 
-for WORKLOAD_OPT in ${WORKLOAD_OPTS[@]}; do
-  for INFERENCE_DEVICE in ${INFERENCE_DEVICE_OPTS[@]}; do
-    python deploy_classification-compile_lib.py ${INFERENCE_DEVICE} cpu_arm64 ${WORKLOAD_OPT} /home/jonask/Repos/tvm-simbricks/mxnet/
+# VTA-based inference
+for BLOCK_OPT in ${BLOCK_OPTS[@]}; do
+  BLOCK_OPT_LOG=$(echo $BLOCK_OPT | awk '{print log($1)/log(2)}')
+  cat > /workspaces/tvm-acdsim/3rdparty/vta-hw/config/vta_config.json <<EOF
+{
+      "TARGET": "simbricks-pci",
+      "HW_VER": "0.0.2",
+      "LOG_INP_WIDTH": 3,
+      "LOG_WGT_WIDTH": 3,
+      "LOG_ACC_WIDTH": 5,
+      "LOG_BATCH": 0,
+      "LOG_BLOCK": ${BLOCK_OPT_LOG},
+      "LOG_UOP_BUFF_SIZE": 15,
+      "LOG_INP_BUFF_SIZE": 15,
+      "LOG_WGT_BUFF_SIZE": 18,
+      "LOG_ACC_BUFF_SIZE": 17
+}
+EOF
+  for INFERENCE_DEVICE in ${INFERENCE_DEVICE_VTA_OPTS[@]}; do
+    for WORKLOAD_OPT in ${WORKLOAD_OPTS[@]}; do
+      python deploy_classification-compile_lib.py ${INFERENCE_DEVICE} cpu_arm64 ${WORKLOAD_OPT} /workspaces/tvm-acdsim/mxnet/
+    done
   done
 done
 
+# CPU-based inference
+for INFERENCE_DEVICE in ${INFERENCE_DEVICE_CPU_OPTS[@]}; do
+  for WORKLOAD_OPT in ${WORKLOAD_OPTS[@]}; do
+    python deploy_classification-compile_lib.py ${INFERENCE_DEVICE} cpu_arm64 ${WORKLOAD_OPT} /workspaces/tvm-acdsim/mxnet/
+  done
+done
